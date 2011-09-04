28f9457673dac3e7f8b217ab7c39f2724521aeb2
Pawel Lorek <plorek@neubloc.com>
2011-09-04 23:51:26 +0200

    Small refactor - draw() was growing too big

--- javascript/smiley.js
+++ javascript/smiley.js
@@ -24,8 +24,10 @@ sB.smiley = (function(window, undefined){
       }
     }();
 
-    var draw = function(){
-      var ctx = context();
+    var drawBackground = function(){
+      var ctx = context(), 
+        radgrad1, //dark stuff at the border
+        radgrad2; //light bouncing of the siede
       ctx.beginPath();  
       var radgrad1 = ctx.createRadialGradient(75, 75, 40, 75, 75, 50);  
       radgrad1.addColorStop(0, '#FFFF00');  
@@ -33,9 +35,10 @@ sB.smiley = (function(window, undefined){
       radgrad1.addColorStop(0.8, '#BBBB00');  
       radgrad1.addColorStop(1, '#999900');  
       ctx.fillStyle = radgrad1;
-      ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle  
+      ctx.arc(75,75,50,0,Math.PI*2,true);
       ctx.fill();
       ctx.closePath();
+
       ctx.beginPath();  
       var radgrad2 = ctx.createRadialGradient(50, 50, 0, 50, 50, 20);  
       radgrad2.addColorStop(0, '#FFFFFF');  
@@ -43,11 +46,13 @@ sB.smiley = (function(window, undefined){
       radgrad2.addColorStop(1, '#FFFF00');  
       ctx.closePath();
       ctx.fillStyle = radgrad2;
-      ctx.arc(75, 75, 40,0,Math.PI*2,true); // Outer circle  
+      ctx.arc(75, 75, 40,0,Math.PI*2,true);
       ctx.fill();
       ctx.closePath();
-      
-      ctx.fillStyle = "black"; 
+    }
+
+    drawFace = function(){
+      var ctx = context();
       ctx.beginPath();  
       ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle  
       ctx.moveTo(110,75);  
@@ -56,6 +61,7 @@ sB.smiley = (function(window, undefined){
       ctx.stroke(); 
       ctx.closePath();
 
+      ctx.fillStyle = "black"; 
       ctx.save();
       ctx.scale(0.3, 1);
       ctx.beginPath();
@@ -64,6 +70,10 @@ sB.smiley = (function(window, undefined){
       ctx.fill();
       ctx.closePath();
       ctx.restore();
+    }
+    var draw = function(){
+      drawBackground();
+      drawFace();
     }
 
     return {
