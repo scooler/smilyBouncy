753de574e58d8768778e51d49382567f1a029850
Pawel Lorek <plorek@neubloc.com>
2011-09-04 21:54:37 +0200

    Fixing - dumb copy paste and no test - that's the way to go :D

--- javascript/initializer.js
+++ javascript/initializer.js
@@ -19,15 +19,15 @@
 
   init = function(){
     var context = getContext();
-    ctx.beginPath();  
-    ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle  
-    ctx.moveTo(110,75);  
-    ctx.arc(75,75,35,0,Math.PI,false);   // Mouth (clockwise)  
-    ctx.moveTo(65,65);  
-    ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye  
-    ctx.moveTo(95,65);  
-    ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye  
-    ctx.stroke(); 
+    context.beginPath();  
+    context.arc(75,75,50,0,Math.PI*2,true); // Outer circle  
+    context.moveTo(110,75);  
+    context.arc(75,75,35,0,Math.PI,false);   // Mouth (clockwise)  
+    context.moveTo(65,65);  
+    context.arc(60,65,5,0,Math.PI*2,true);  // Left eye  
+    context.moveTo(95,65);  
+    context.arc(90,65,5,0,Math.PI*2,true);  // Right eye  
+    context.stroke(); 
   };
 
   sB.getCanvas = getCanvas;
